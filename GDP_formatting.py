86aee2b87afa3dd889aac9d9dc8e92f034f6d717
Cory Knox <cknox0119@gmail.com>
2020-11-08 00:33:35 -0500

    python files cleaned for submission

--- GDP_formatting.py
+++ GDP_formatting.py
@@ -7,15 +7,27 @@ import matplotlib.ticker as plticker
 import csv
 
 def get_csv(url):
+    """
+    return the imported data from a site
+    Args:
+        url: string to find site
+    Returns:
+        data that is ready to be formatted further, currently a csv file
+    """
     input_file = requests.get(url)
-    # remove the first rows that are not the majority character length...
-
     return input_file
 
 # function formatting csv files:
 def format_csv_files(input_file):
+    """
+    reformat the csv file, removing lines that are less than 66 characters as they interrupt pandas.
+    remove the unecessary columns of information.
+    Args:
+        input_file: original data, unformatted, including the short lines
+    Returns:
+        data with consistent lengths amongst strings to be read by pandas
+    """
     csv_data = pd.read_csv(input_file, sep=";")
-    
     with open(input_file) as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         for row in readCSV:
@@ -24,37 +36,64 @@ def format_csv_files(input_file):
                 print()
 
     csv_data = csv_data.drop(columns=['Country Code', 'Indicator Name', 'Indicator Code'])
-    # column_names = csv_data.columns.values
     return csv_data
 
 def create_column_names(csv_data):
+    """
+    create a list of column names from the formatted data
+    Args:
+        csv_data: formatted data of GDP information
+    Returns:
+        list of column name values
+    """
     column_names = csv_data.columns.values
     return column_names
 
 def find_years(column_names):
+    """
+    establish the years-span of the data and create the years into a list of strings.
+    Args:
+        column_names: list of strings that are column names of csv data
+    Returns:
+        list of strings that are purely numerical years
+    """
     years = column_names[1:-1]
     return years
 
 def data_country(country_name, csv_data):
+    """
+    find the data applicable for one specific country of interest
+    Args:
+        country_name: string input that allows user to clarify the name of the country to search
+        csv_data: cleaned, reformatted dataframe of csv data
+    Returns:
+        series of the row of data for the specified country
+    """
     data_country = csv_data.loc[csv_data['Country']== country_name]
     return data_country
     
-def create_country_plot(data_country, column_titles, years, country_name, x_label, y_label, plot_title):
-    # row_int = data_country.loc[data_country["Country Name"]== country_name]
-    row_number = data_country.loc[data_country["Country Name"] == country_name].index
-    row_to_graph = data_country.iloc[row_number] ## gets specific rows data --- How would I go about plotting this?
-    
-    row_data = row_to_graph.values[1:-1]
-    # row_idx = row_to_graph.index[1:-1]
+def create_country_plot(data_country, country_name):
+    """
+    create the plot of the specified country
+    Args:
+        data_country: reformatted data for specific country
+        country_name: name of the country of interest to search in dataframe
+    Returns:
+        plot of the adoption data of the specified country
+    """
+    row_number = data_country.loc[data_country["Country"] == country_name].index.values[0]
+    row_to_graph = data_country.iloc[row_number] 
 
-    # scatter plot formatting:
+    row_data = row_to_graph.values[1:-1]
+    years = row_to_graph.index[1:-1]
+
     fig, axs = plt.subplots()
     axs.scatter(years, row_data)
+    axs.set_xticklabels(format_column_names(years))
     plt.xticks(rotation=90)
-    loc = plticker.MultipleLocator(base=5) # this locator puts ticks at regular intervals
+    loc = plticker.MultipleLocator(base=5)
     axs.xaxis.set_major_locator(loc)
-
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.title(plot_title)
-    
+    plt.xlabel("Time (years)")
+    plt.ylabel("Annuel GDP")
+    plt.title(country_name + " GDP vs Time")
+    plt.show()
--- adoption_formatting.py
+++ adoption_formatting.py
@@ -1,4 +1,3 @@
-# %matplotlib inline
 import requests
 import json
 import matplotlib.pyplot as plt
@@ -6,35 +5,36 @@ import pandas as pd
 import numpy as np
 import matplotlib.ticker as plticker
 
-
-# FUNCTIONS TO MAKE:
-# get the data with the URL input and format in json --- features to the data to be analyzed + establish column names
-# format column names of data set 
-# country data to DataFrame
-# data of one country returning the series
-# plot in other .py
-
-
-
-# request data and initally format because of json formatting,
-# establish beginning variables:
 def get_data(url):
     """
     return the imported data from a site, originally in json formatting.
     Args:
         url: string to find site
     Returns:
-        data that is ready to be formatted further...(erwrite later)
+        data that is ready to be formatted further, currently in json formatting
     """
     data = requests.get(url)
     data = data.json()
     return data["features"]
 
 def get_column_names(data):
+    """
+    find the attributes of the original data collected
+    Args:
+        data: unformatted, original data that was pulled from above
+    Returns:
+        attributes of the data collected
+    """
     return data[0]["attributes"].keys()
 
-# format the column names, getting rid of unecessary characters
 def format_column_names(column_names):
+    """
+    format the column names, getting rid of unecessary characters
+    Args:
+        column_names: a list of the original column names from data collected
+    Returns:
+        list of column names with unecessary characters removed from strings
+    """
     new_column_names = []
     for each in column_names:
         temp = each.split("_")
@@ -44,8 +44,15 @@ def format_column_names(column_names):
             new_column_names.append(temp[0])
     return new_column_names
 
-# put the data into a dataframe:
 def data_to_dataframe(data, column_names):
+    """
+    formatting the attributes of the data into an appropriate dataframe with pandas
+    Args:
+        data: original data attributes
+        column_names: list of column name strings
+    Returns:
+        dataframe of cleaned, reformatted data
+    """
     country_data_formatted = []
     for each in data:
         country_data_formatted.append(each["attributes"])
@@ -57,36 +64,41 @@ def data_to_dataframe(data, column_names):
 
     return adoption_dataframe
 
-# find the data for one specific country:
 def data_for_country(country_name, adoption_dataframe):
+    """
+    find the data applicable for one specific country of interest
+    Args:
+        country_name: string input that allows user to clarify the name of the country to search
+        adoption_dataframe: cleaned, reformatted dataframe of adoption data
+    Returns:
+        series of the row of data for the specified country
+    """
     return adoption_dataframe.loc[adoption_dataframe['Country'] == country_name]
 
 def create_country_plot(data_country, country_name):
-    # row_int = data_country.loc[data_country["Country Name"]== country_name]
+    """
+    create the plot of the specified country
+    Args:
+        data_country: reformatted data for specific country
+        country_name: name of the country of interest to search in dataframe
+    Returns:
+        plot of the adoption data of the specified country
+    """
     row_number = data_country.loc[data_country["Country"] == country_name].index.values[0]
-    print(type(row_number))
-    print(row_number)
     row_to_graph = data_country.iloc[row_number] 
 
     row_data = row_to_graph.values[1:-1]
     years = row_to_graph.index[1:-1]
 
-    print(type(row_to_graph))
-
     fig, axs = plt.subplots()
     axs.scatter(years, row_data)
     axs.set_xticklabels(format_column_names(years))
-
     plt.xticks(rotation=90)
-
-    # loc = plticker.MultipleLocator(base=5)
-    # axs.xaxis.set_major_locator(loc)
-
     plt.xlabel("Time (years)")
     plt.ylabel("Annuel Adoptions")
     plt.title(country_name + " Adoptions vs Time")
-
     plt.show()
+
 
 if __name__ == "__main__":
     adoption_url = "https://services6.arcgis.com/R6wlO6UHmSzqm9Vs/arcgis/rest/services/Country_Adoptions_by_Year/FeatureServer/0/query?f=json&where=1=1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Country%20asc&resultOffset=0&resultRecordCount=2599&resultType=standard&cacheHint=true"
